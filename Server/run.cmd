740fda7bd5768ac4078a75bad458598f578c5fec
jc <jc@gmail.com>
2017-03-29 02:30:00 +0800

    change copy paht

--- Server/run.cmd
+++ Server/run.cmd
@@ -1,9 +1,9 @@
 del bin\*.*
 copy ..\Regulus.Project.Chat.Game\bin\Debug\*.* bin\*.*
-copy ..\..\Regulus\Tool\Server\bin\debug\*.* bin\*.*
-copy ..\..\Regulus\Tool\GhostProviderGenerator\bin\debug\*.* bin\*.*
+copy ..\Regulus\Tool\Server\bin\debug\*.* bin\*.*
+copy ..\Regulus\Tool\GhostProviderGenerator\bin\debug\*.* bin\*.*
 cd bin
-GhostProviderGenerator.exe ..\generate.ini
+RegulusRemoteProtocolGenerator.exe ..\generate.ini
 server.exe launchini ..\config.ini
 
 pause
