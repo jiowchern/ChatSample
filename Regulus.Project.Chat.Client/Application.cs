00ce768be58e4db874a0d25266d9fa01b0caf397
JC <jiowchern@yahoo.com.tw>
2019-06-13 19:02:35 +0800

    新增client

---
+++ Regulus.Project.Chat.Client/Application.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Regulus.Utility.WindowConsoleAppliction;
+namespace Regulus.Project.Chat.Client
+{
+    public class Application
+    {
+
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
